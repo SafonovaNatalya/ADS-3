d505f251e29c421f65271f5e074f8c6c4893a622
SafonovaNatalya <116429612+SafonovaNatalya@users.noreply.github.com>
2023-03-17 13:48:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int starting = 0;
+    int ending = size - 1;
+    int kol = 0;
+    while (starting <= ending) {
+        int i = (starting + ending) / 2;
+        if (arr[i] == value) {
+            kol++;
+            for (int j = i + 1; j <= size && arr[j] == value; j++) {
+                kol++;
+            }
+            for (int j = i - 1; j >= 0 && arr[j] == value; j--) {
+                kol++;
+            }
+            return kol;
+        }
+        if (arr[i] > value) ending = i - 1;
+        if (arr[i] < value) starting = i + 1;
+    }
+  return 0;
 }
